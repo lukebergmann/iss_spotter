32f67ad8f5aadfc85bfc2ae586588fb39151fb8b
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-12 06:25:02 +0000

    created fetch coords by ip

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
 fetchCoordsByIP('24.64.125.127', (error, coordinates) => {
   if (error) {
